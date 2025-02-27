68bba7ceb547510cd645b9b7b0827cb269e2b742
Mario Caballero Ramírez <mariocaballero@siani.es>
2025-02-27 09:52:13 +0000

    runtime: added state to header

--- runtime/src/io/picota/runtime/ui/displays/templates/HeaderTemplate.java
+++ runtime/src/io/picota/runtime/ui/displays/templates/HeaderTemplate.java
@@ -2,6 +2,8 @@ package io.picota.runtime.ui.displays.templates;
 
 import io.picota.runtime.RuntimeBox;
 
+import java.util.Set;
+
 public class HeaderTemplate extends AbstractHeaderTemplate<RuntimeBox> {
 
 	public HeaderTemplate(RuntimeBox box) {
@@ -12,5 +14,7 @@ public class HeaderTemplate extends AbstractHeaderTemplate<RuntimeBox> {
 	public void refresh() {
 		super.refresh();
 		title.value(box().configuration().title());
+		state.formats(Set.of("stateStyle", "state" + box().state().name() + "Style"));
+		stateMessage.value(translate(box().state().name()));
 	}
 }
