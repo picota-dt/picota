c771cd5d48656f7bc8b71e9c8b4b625b53a0b2e9
Octavio Roncal <octavioroncal@monentia.com>
2025-06-26 15:23:01 +0100

    test

--- digital-twin/test/TrainDataPreparationTest.java
+++ digital-twin/test/TrainDataPreparationTest.java
@@ -21,7 +21,7 @@ public class TrainDataPreparationTest {
 		PicotaGraph graph = model.graph();
 		File dir = new File("../temp/test");
 		FileUtils.deleteDirectory(dir);
-		TrainDataPreparer preparer = new TrainDataPreparer(new Archetype(dir), digitalTwin);
+		TrainDataPreparer preparer = new TrainDataPreparer(new Archetype(dir), null);
 		DigitalTwin.DigitalSubject ds = graph.digitalTwin().digitalSubject(0);
 		File subjectDataset = new File("/Users/oroncal/workspace/projects/picota/digital-twin/test-res/example/lite/SolarPlant.csv");
 		preparer.prepareData(ds, ds.inferenceModel(0), subjectDataset);
